d9c505ef4a61a9842df661913c0323c84c20c596
Deepak <deepakpandey1234@gmail.com>
2017-05-20 17:50:07 +0530

    POC

---
+++ src/A.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by panded4 on 3/30/2017.
+ */
+public class A implements Comparator{
+
+  /*  static String concat(String[] strings) {
+        String s="";
+        for(int i=0;i<strings.length;i++){
+
+        }
+    }*/
+
+    public static void main(String[] args) {
+        String[] a = { "f", "o", "o", "bar" };
+    }
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
+}
